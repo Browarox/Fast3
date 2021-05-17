bdb8b2576f2d8fb7aee8bc459271b13a6f2c4fc4
Browarox <1996beryl@gmail.com>
2021-05-17 11:22:14 +0200

    a

--- app/app/database.py
+++ app/app/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 
-engine = create_engine("postgresql://postgres:DaftAcademy@127.0.0.1:5555/postgres")
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
